bedd1535f5c3bd5052c487a7ab22d11e976fa8cd
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-10 19:59:54 -0400

    added new page

--- controllers/bread.js
+++ controllers/bread.js
@@ -5,10 +5,27 @@ router.get('/', (req, res) => {
     res.render('index', { breads: Bread })
 })
 
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
 //Get bread by index
 router.get('/:index', (req, res) => {
     const { index } = req.params
     res.render('show', { bread: Bread[index] })
 })
+
+
+router.post('/', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.bodyhasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+
+    Bread.push(req.body)
+    res.redirect('/breads')
+})
+
 
 module.exports = router
